57667b038da94f961bbfad2d16e283cf73c15ea2
Plumblumpb <2573350997@qq.com>
2018-07-31 15:32:41 +0800

    自动生成代码

---
+++ src/main/java/com/plumblum/mybatisgenetor/MybatisgenetorApplication.java
@@ -0,0 +1,12 @@
+package com.plumblum.mybatisgenetor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisgenetorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisgenetorApplication.class, args);
+	}
+}
